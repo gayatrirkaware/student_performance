3335720cef52c2bc5ffbe93c89a9ec2452acc029
gayatrirkaware <gayatrikawarer@gmail.com>
2025-05-03 17:18:57 +0530

    Added project configuration file

---
+++ project_config.py
@@ -0,0 +1,8 @@
+CSV_FILE_PATH = r"Data\Student_Performance.csv"
+MODEL_FILE_PATH = r"Artifacts\linear_regression.pkl"
+LABEL_ENC_DATA_PATH = r"Artifacts\label_enc_data.json"
+FLASK_PORT_NUMBER = 8086
+MONGODB_PORT_NUMBER = 27017
+PROJECT_DB_NAME = "student_performance_db"
+USER_CRED_COLLECTION_NAME = 'user_details'
+TESTING_COLLECTION_NAME = 'student_performance_data'
